0f3e6e3766cdec6e3b5bd4b4b1d0f43e8aa6ff4d
Lawrence Zhou <60532041+illonion@users.noreply.github.com>
2025-04-30 19:21:39 +1000

    Star display

--- main/index.js
+++ main/index.js
@@ -1,6 +1,7 @@
 // Get Beatmaps
 const roundNameEl = document.getElementById("round-name")
-let allBeatmaps, currentBestOf, currentFirstTo
+let currentBestOf, currentFirstTo, currentLeftStars = 0, currentRightStars = 0
+let allBeatmaps
 async function getBeatmaps() {
     const response = await fetch("../_data/beatmaps.json")
     const responseJson = await response.json()
@@ -16,8 +17,44 @@ async function getBeatmaps() {
             currentBestOf = 13; break;
     }
     currentFirstTo = Math.ceil(currentBestOf / 2)
+
+    createStarDisplay()
 }
 getBeatmaps()
+
+// Team Stars
+const leftTeamStarContainerEl = document.getElementById("left-team-star-container")
+const rightTeamStarContainerEl = document.getElementById("right-team-star-container")
+function createStarDisplay() {
+    leftTeamStarContainerEl.innerHTML = ""
+    rightTeamStarContainerEl.innerHTML = ""
+
+    let i = 0
+    for (i; i < currentLeftStars; i++) createStar("green", "fill")
+    for (i; i < currentFirstTo; i++) createStar("green", "empty")
+    i = 0
+    for (i; i < currentRightStars; i++) createStar("blue", "fill")
+    for (i; i < currentFirstTo; i++) createStar("blue", "empty")
+
+
+    function createStar(colour, status) {
+        const wrapper = document.createElement("div")
+        wrapper.classList.add("team-star-wrapper")
+
+        let image
+        if (status === "fill") {
+            image = document.createElement("img")
+            image.setAttribute("src", `static/${colour} star.png`)
+        } else {
+            image = document.createElement("div")
+            image.classList.add("no-star", (colour === "green")? "no-left-star" : "no-right-star")
+        }
+
+        wrapper.append(image)
+        if (colour === "green") leftTeamStarContainerEl.append(wrapper)
+        else rightTeamStarContainerEl.append(wrapper)
+    }
+}
 
 // Team Name
 const leftTeamNameEl = document.getElementById("left-team-name")
